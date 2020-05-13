7e00a18beaf7dd3e1b81487eac904f6fd4771456
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2020-05-13 13:00:23 +0530

    Delete bookmark route added

--- routes/streaming.js
+++ routes/streaming.js
@@ -1,27 +1,28 @@
-var express  = require("express"),
-	mongoose = require("mongoose"),
-    router   = express.Router({mergeParams: true}),
-	Media    = require("../models/media"),
-	User     = require("../models/user"),
-
-    fs       = require('fs');
+var express              = require("express"),
+	mongoose             = require("mongoose"),
+    router               = express.Router({mergeParams: true}),
+	Media                = require("../models/media"),
+	User                 = require("../models/user"),
+	Course               = require("../models/course"),
+    fs                   = require('fs');
 
 router.get("/video/:video_id", async(req, res, next)=>{
 
 	try{
+		let getcourse=Course.findOne({'_id':req.params.id})
 		let getvideo= Media.findOne({'_id':req.params.video_id,'course':req.params.id})
 		let getuser= User.findOne({'_id':req.user._id,'enrolled_courses.course':req.params.id})
-		let [user,video]=await Promise.all([getuser,getvideo])
+		let [user,video, course]=await Promise.all([getuser,getvideo, getcourse])
 
 		if(user && video){
 			let courseIndex=user.enrolled_courses.findIndex(courseItem=>{
 				return courseItem.course==req.params.id
 			  })
 
-			  console.log(user.enrolled_courses[courseIndex])
+			  //console.log(user.enrolled_courses[courseIndex])
 
 			  //let foundVideo=Media.findById(req.params.video_id)
-			  res.render("video", {video:video, course_id:req.params.id, bookmarks:user.enrolled_courses[courseIndex].Bookmarks})
+			  res.render("video", {video:video, course:course, bookmarks:user.enrolled_courses[courseIndex].Bookmarks})
 		}
 		else{
 			error={'status':400,'message':'Course-video mismatch'}
@@ -84,38 +85,85 @@ router.get('/video/watch/:video_id',async (req,res)=>{
 })
 
 router.post('/video/:video_id/', async (req,res,next)=>{
-	//check authenticated
-	//check course enrolled
-	//check valid video
-	try{
-	  let getvideo= Media.findOne({'_id':req.params.video_id,'course':req.params.id})
-	  let getuser= User.findOne({'_id':req.user._id,'enrolled_courses.course':req.params.id})
-	  let [user,video]=await Promise.all([getuser,getvideo])
-	  //debugger;
-	  if(user && video){ 
-		let courseIndex=user.enrolled_courses.findIndex(courseItem=>{
-		  return courseItem.course==req.params.id
-		})
-		
-		let newBookmark={
-			'video':mongoose.Types.ObjectId(req.params.video_id),
-			'timestamp':req.body.time,
-			'text':req.body.text
-		  }
 
-		user.enrolled_courses[courseIndex].Bookmarks.push(newBookmark)
+	try{
+		let getcourse=Course.findOne({'_id':req.params.id})
+		let getvideo= Media.findOne({'_id':req.params.video_id,'course':req.params.id})
+		let getuser= User.findOne({'_id':req.user._id,'enrolled_courses.course':req.params.id})
+		let [user,video, course]=await Promise.all([getuser,getvideo, getcourse])
+		//debugger;
+		if(user && video){ 
+		  let courseIndex=user.enrolled_courses.findIndex(courseItem=>{
+			return courseItem.course==req.params.id
+		  })
+		  
+		  let newBookmark={
+			  'video':mongoose.Types.ObjectId(req.params.video_id),
+			  'timestamp':req.body.time,
+			  'text':req.body.text
+			}
   
-		let updated= await user.save()
-		res.json(newBookmark)
+		  user.enrolled_courses[courseIndex].Bookmarks.push(newBookmark)
+	
+		  let updated= await user.save()
+		  res.json({
+			bookmark:newBookmark,
+			video:video,
+			course:course})
+		}
+		else{
+		  error={'status':400,'message':'Course-video mismatch'}
+		  throw error
+		}
 	  }
-	  else{
-		error={'status':400,'message':'Course-video mismatch'}
-		throw error
+	  catch(err){
+		next(err)
 	  }
-	}
-	catch(err){
-	  next(err)
-	}
-  })
+})
+
+router.delete('/video/:video_id/bookmark/:book_id/', async(req, res, next)=>{
+	try{
+		let getvideo= Media.findOne({'_id':req.params.video_id,'course':req.params.id})
+		let getuser= User.findOne({'_id':req.user._id,'enrolled_courses.course':req.params.id})
+		let [user,video]=await Promise.all([getuser,getvideo])
+		//debugger;
+		if(user && video){ 
+		  let courseIndex=user.enrolled_courses.findIndex(courseItem=>{
+			return courseItem.course==req.params.id
+		  })
+  
+		  var index=user.enrolled_courses[courseIndex].Bookmarks.findIndex(bookmark=>{
+			  return bookmark._id.equals(req.params.book_id);
+		  })
+
+		  var foundBookmark=user.enrolled_courses[courseIndex].Bookmarks.filter(bookmark=>{
+			  if(bookmark._id.equals(req.params.book_id)){
+				  return bookmark;
+			  }
+		  })
+
+		  if(index>-1){
+			user.enrolled_courses[courseIndex].Bookmarks.splice(index, 1);
+		  }
+		  
+		  
+		  let updated= await user.save()
+	
+		  //console.log(foundBookmark);
+		  res.json(foundBookmark)
+		}
+		else{
+		  error={'status':400,'message':'Course-video mismatch'}
+		  throw error
+		}
+	  }
+	  catch(err){
+		next(err)
+	  }
+
+		//res.send("You have reached delete route")
+	
+	
+})
 
 module.exports = router;
