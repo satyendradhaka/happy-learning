d117816cef6b97e11b63c6f570761c6bbcfe08e7
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2020-05-25 12:46:24 +0530

    multiple file upload added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,19 +1,23 @@
-let express = require('express')
-let router = express.Router()
-let multer = require('multer')
-let Course = require("../models/course")
-let Media = require("../models/media")
-let User = require("../models/user")
+let express = require("express");
+let router = express.Router();
+let multer = require("multer");
+let Course = require("../models/course");
+let Media = require("../models/media");
+let User = require("../models/user");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './assets');
+    cb(null, "./assets");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage }).fields([
+  { name: "v720" },
+  { name: "v480" },
+  { name: "v360" },
+]);
 
 //home page route for admin
 router.get("/", isAdmin, function (req, res) {
@@ -53,7 +57,6 @@ router.get("/courses", isAdmin, function (req, res) {
   });
 });
 
-
 //add a new courses form render
 router.get("/courses/add", isAdmin, function (req, res) {
   res.render("./admin/courses/new");
@@ -70,8 +73,8 @@ router.post("/courses", isAdmin, function (req, res) {
     } else {
       res.redirect("/admin/courses");
     }
-  })
-})
+  });
+});
 
 //view a specifc course
 router.get("/courses/:id", isAdmin, function (req, res) {
@@ -79,10 +82,10 @@ router.get("/courses/:id", isAdmin, function (req, res) {
     if (err) {
       res.redirect("/admin/courses");
     } else {
-      console.log(foundCourse)
+      console.log(foundCourse);
       res.render("./admin/courses/show", { course: foundCourse });
     }
-  })
+  });
 });
 
 //add a new video to the course
@@ -94,91 +97,102 @@ router.get("/courses/:id/video/add", isAdmin, function (req, res) {
     } else {
       res.render("./admin/courses/videos/add", { course: foundCourse });
     }
-  })
+  });
 });
 //logic for handeling adding of new video
-router.post("/courses/:id", isAdmin, upload.single('video'), function (req, res) {
-  Course.findById(req.params.id, function (err, foundCourse) {
+router.post("/courses/:id", isAdmin, function (req, res, next) {
+  upload(req, res, function (err) {
     if (err) {
-      res.redirect("/admin/courses");
+      console.log(err);
     } else {
-      var media = {
-        title: req.body.title,
-        filePath: req.file.destination + "/"+req.file.originalname
-      }
-      Media.create(media, function (err, newlyCreated) {
-        if (err) {
-          console.log(err);
-        } else {
-          //add course id to media
-          newlyCreated.course = req.params.id;
-          //save media
-          newlyCreated.save();
-          foundCourse.videos.push(newlyCreated);
-          foundCourse.save();
-          res.redirect("/admin/courses/" + req.params.id);
-        }
-      })
-    }
-  })
-});
+      Course.findById(req.params.id, function (err, foundCourse) {
+        if (err) throw err;
 
+        let media = {
+          title: req.body.title,
+          filePath1: req.files["v720"][0].destination + "/" + req.files["v720"][0].originalname,
+          filePath2: req.files["v480"][0].destination + "/" + req.files["v480"][0].originalname,
+          filePath3: req.files["v360"][0].destination + "/" + req.files["v360"][0].originalname,
+        };
+
+
+        Media.create(media, function (err, newlyCreated) {
+          if (err) {
+            console.log(err);
+          } else {
+            //add course id to media
+            newlyCreated.course = req.params.id;
+            //save media
+            newlyCreated.save();
+            foundCourse.videos.push(newlyCreated);
+            foundCourse.save();
+            res.redirect("/admin/courses/" + req.params.id);
+          }
+        });
+      });
+    }
+  });
+});
 
 //edit course
 //route for edit course form
 router.get("/courses/:id/edit", isAdmin, function (req, res) {
   Course.findById(req.params.id, function (err, foundCourse) {
     res.render("./admin/courses/edit", { course: foundCourse });
-  })
-})
+  });
+});
 //update course
 router.put("/courses/:id", isAdmin, function (req, res) {
-  Course.findByIdAndUpdate(req.params.id, req.body.course, function (err, updatedCourse) {
+  Course.findByIdAndUpdate(req.params.id, req.body.course, function (
+    err,
+    updatedCourse
+  ) {
     if (err) {
       res.redirect("/");
     } else {
       res.redirect("/admin/courses");
     }
-  })
-})
+  });
+});
 
 //view all users
-router.get('/users', isAdmin, async (req, res, next) => {
+router.get("/users", isAdmin, async (req, res, next) => {
   try {
-    let users = await User.find()
+    let users = await User.find();
     if (users.length) {
-      res.send(users)
-    }
-    else {
+      res.send(users);
+    } else {
       error = {
-        'status': 400,
-        'message': "No users found"
-      }
-      throw error
+        status: 400,
+        message: "No users found",
+      };
+      throw error;
     }
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-})
+});
 
 //edit user permissions
 //route for edit user form
 router.get("/users/:id/edit", isAdmin, function (req, res) {
   User.findById(req.params.id, function (err, foundUser) {
     res.render("./admin/users/edit", { user: foundUser });
-  })
-})
+  });
+});
 //update user permissions
 router.put("/users/:id", isAdmin, function (req, res) {
-  User.findByIdAndUpdate(req.params.id, req.body.user, function (err, updatedUser) {
+  User.findByIdAndUpdate(req.params.id, req.body.user, function (
+    err,
+    updatedUser
+  ) {
     if (err) {
       res.redirect("/");
     } else {
       res.redirect("/admin/users");
     }
-  })
-})
+  });
+});
 
 //middleware
 function isAdmin(req, res, next) {
@@ -189,6 +203,5 @@ function isAdmin(req, res, next) {
   }
   res.redirect("/");
 }
-
 
 module.exports = router;
