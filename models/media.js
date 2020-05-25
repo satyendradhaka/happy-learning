c9f60a4dde7f7a00ebc3c8aae68a298876bfe606
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2020-05-25 12:41:04 +0530

    updated file paths

--- models/media.js
+++ models/media.js
@@ -2,10 +2,14 @@ var mongoose = require("mongoose");
 let Schema = mongoose.Schema
 
 let mediaSchema = new Schema({
+	
 	title: { type: String },
-	filePath: { type: String },
+	filePath1: { type: String }, //720p
+	filePath2: { type: String }, //480p
+	filePath3: { type: String }, //360p
+
 	course: { type: mongoose.ObjectId, ref: 'Course' },
-	subtopic: { type: String, required: true},
+	//subtopic: { type: String, required: true},
 	viewcount: { type: Number, default: 0 }
 })
 
