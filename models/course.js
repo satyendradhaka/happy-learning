d165df89a23051e2036e3c460f6f06d00b01138b
Dhaka <satyendradhaka5@gmail.com>
2020-08-08 19:11:56 +0530

    add description

--- models/course.js
+++ models/course.js
@@ -3,8 +3,9 @@ let Schema = mongoose.Schema
 
 let courseSchema = new Schema({
 	title: { type: String, required: true },
-	//author: {type: String, required: true},
+	author: {type: String, required: true},
 	topics: [String],
+	description: String,
 	videos: [{ type: mongoose.ObjectId, ref: 'Media' }]
 })
 
